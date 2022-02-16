c05b24b5193586bbe055ac1ff253cfca7024addb
lythanhhai <you@example.com>
2022-02-16 22:43:05 +0700

    study about serializer, models in django

--- pythonWeb/Polls/admin.py
+++ pythonWeb/Polls/admin.py
@@ -1,3 +1,14 @@
+from csv import list_dialects
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+
+#admin.site.register(Article)
+
+@admin.register(Article)
+class ArticleModel(admin.ModelAdmin):
+    # lọc theo title và description
+    list_filter = ('title', 'description')
+    #
+    list_display = ('title', 'description')
