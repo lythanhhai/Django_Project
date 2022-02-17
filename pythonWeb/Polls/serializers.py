de1f94a8bfcf0be2f13b0baeffc0f9dd818bef05
lythanhhai <you@example.com>
2022-02-17 23:51:12 +0700

    study about serializer and manipulated with api

--- pythonWeb/Polls/serializers.py
+++ pythonWeb/Polls/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Article
 
+'''
 class ArticleSerializers(serializers.Serializer):
     title = serializers.CharField(max_length=100)
     description = serializers.CharField(max_length=400)
@@ -11,3 +12,10 @@ class ArticleSerializers(serializers.Serializer):
     def update(seft, instance, validated_data):
         instance.title = validated_data.get('title', instance.title)
         instance.description = validated_data.get('description', instance.description)
+'''
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        #fields = '__all__'
+        fields = ['id', 'title', 'description']
--- pythonWeb/Polls/urls.py
+++ pythonWeb/Polls/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import Index
+#from .views import Index
+from .views import article_list
 
 
 urlpatterns = [
-    path('', Index),
+    #path('', Index),
+    path('articles/', article_list)
 ]
